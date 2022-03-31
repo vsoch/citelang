59d3ac8d138f4faab78823a09fc1cc0a1d57da3e
vsoch <vsoch@users.noreply.github.com>
2022-03-30 22:37:41 -0600

    wrong cache path
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- citelang/client/cache.py
+++ citelang/client/cache.py
@@ -2,15 +2,16 @@ __author__ = "Vanessa Sochat"
 __copyright__ = "Copyright 2022, Vanessa Sochat"
 __license__ = "MPL 2.0"
 
-from citelang.main import Client
+from citelang.main.cache import cache
+from citelang.main.settings import settings
 
 
 def main(args, parser, extra, subparser):
 
     # init global settings
-    cli = Client(quiet=args.quiet)
+    cli = cache.cache
 
     if args.clear:
-        cli.clear_cache(force=args.force)
+        cli.clear(force=args.force)
     else:
-        print(cli.settings.cache_dir)
+        print(settings.cfg.cache_dir)
