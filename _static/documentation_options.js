17c985f07c605227a60132abc027978e6e84ede2
vsoch <814322+vsoch@users.noreply.github.com>
2025-01-20 21:17:40 +0000

    Deploying to gh-pages from @ vsoch/citelang@bbbec01638368184adc5554f437b7aa97160bb2c 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.34',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.0.35',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
